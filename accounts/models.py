27bcc5541f1ab357fb0f3bc419f66d0a4fe46512
ali <ali.afkhamiii@gmail.com>
2024-12-22 00:18:32 +0330

    fix: add custom manager to User and fix user creation methods

--- accounts/models.py
+++ accounts/models.py
@@ -2,30 +2,27 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from django.db import models
-from django.apps import apps
 
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email, username, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError('email must be set')
 
         email = self.normalize_email(email)
-        GlobalUserModel = apps.get_model(self.model._meta.app_label, self.model._meta.object_name)
-        username = GlobalUserModel.normalize_username()
-        user = self.model(email, username, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, username=None, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email=email, password=password, **extra_fields)
 
-    def create_superuser(self, email, username=None, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -34,7 +31,7 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email=email, password=password, **extra_fields)
 
 
 class User(PermissionsMixin, AbstractBaseUser):
@@ -47,6 +44,7 @@ class User(PermissionsMixin, AbstractBaseUser):
     is_active = models.BooleanField(_("active"), default=True)
 
     USERNAME_FIELD = "email"
+    objects = UserManager()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}" or self.username
